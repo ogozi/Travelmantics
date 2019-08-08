119d57a924c6f0f388025955685aaf6bbb58ce28
Ogozi John <43306392+ogozi@users.noreply.github.com>
2019-08-08 05:39:06 +0100

    Initial commit, will update.

---
+++ app/src/main/java/com/example/travelmantics/FirebaseUtil.java
@@ -0,0 +1,41 @@
+package com.example.travelmantics;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class FirebaseUtil {
+    public static FirebaseDatabase mFirebaseDatabase;
+    public static DatabaseReference mDatabaseReference;
+    private static FirebaseUtil firebaseUtil;
+    public static FirebaseAuth mFirebaseAuth;
+    public static FirebaseAuth.AuthStateListener mAuthListener;
+    public static ArrayList<TravelDeal> mDeals;
+
+    private FirebaseUtil (){};
+
+    public static void openFbReference (String ref){
+        if (firebaseUtil == null){
+            firebaseUtil = new FirebaseUtil();
+            mFirebaseDatabase = FirebaseDatabase.getInstance();
+            mFirebaseAuth = FirebaseAuth.getInstance();
+            mAuthListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                }
+            };
+
+        }
+        mDeals = new ArrayList<TravelDeal>();
+        mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
+
+    }
+
+
+    }
+
